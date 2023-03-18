c7868bf8f3051d5c958f1bbb08d90cccb3762b52
Tigmanshu Garg <114407593+tigmansh@users.noreply.github.com>
2023-03-18 19:07:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("https://localhost:8080/", { transports: ["websocket"] });
+const socket = io("https://chat-application-52xe.onrender.com/", { transports: ["websocket"] });
 
 const form = document.getElementById("chatbox");
 
